4030f3afddc58358fb3a54857dbab499ef8369e4
Mark Grebe <grebe@ucmo.edu>
2020-02-22 20:36:24 -0600

    Check test failure

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO car (make, model, year) VALUES ('Ford', 'Mustang', 1998);
-INSERT INTO car (make, model, year) VALUES ('Acura', 'ILX', 2014);
+INSERT INTO car (make, model, year) VALUES ('Acura', 'ILX', 2015);
 INSERT INTO car (make, model, year) VALUES ('Honda', 'CRV', 2018);
